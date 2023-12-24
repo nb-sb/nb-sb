915a8662f9a67da465eb96f9c0c16dee8e56cb0d
戏人看戏 <94180745+nb-sb@users.noreply.github.com>
2023-12-24 22:28:45 +0800

    Update updata.py

--- updata.py
+++ updata.py
@@ -66,9 +66,12 @@ abstract_tpl = """## Abstract
 <p>
   <img src="https://stats.justsong.cn/api/leetcode/?username=nbsb&theme=light" alt="戏人看戏's LeetCode Stats" width="500px" />
 </p>
+<p>
+  <img src="https://skillicons.dev/icons?i=c,cpp,go,py,html,css,js,nodejs,java,md,pytorch,tensorflow,flask,fastapi,express,qt,react,cmake,docker,git,linux,nginx,mysql,redis,sqlite,githubactions,heroku,vercel,visualstudio,vscode" alt="戏人看戏's LeetCode Stats"  />
+</p>
+
 </a>
 
-![skills](https://skillicons.dev/icons?i=c,cpp,go,py,html,css,js,nodejs,java,md,pytorch,tensorflow,flask,fastapi,express,qt,react,cmake,docker,git,linux,nginx,mysql,redis,sqlite,githubactions,heroku,vercel,visualstudio,vscode)
 
 """
 
